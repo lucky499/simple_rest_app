3593a4b94782774d386272f8a5d7c24ed4062ea9
lucky499 <45645005+lucky499@users.noreply.github.com>
2024-09-24 21:30:49 +0530

    testpy

---
+++ ansible/simple_testing.py
@@ -0,0 +1,9 @@
+# Test on List objects
+my_list = [1,2,3,5]
+print(f"Original: {my_list}")
+
+temp_list = my_list.insert(1, 10)
+print(f"temp_list is: {temp_list}")
+
+pop_list = my_list.pop()
+print(f"pop_list is : {pop_list}")
